f8b5299cdfdc78776130d4cdce1afca1937ecb63
Mathew Power <mathew.power@gmail.com>
2022-05-22 21:50:55 +0100

    Added a dummy function and test

---
+++ asl_toolkit/__init__.py
@@ -0,0 +1,10 @@
+def add_two_integers(a: int, b: int) -> int:
+    """
+    Adds two integers together
+    Args:
+        a: integer a
+        b: integer b
+
+    Returns: The sum of `a` and `b`.
+    """
+    return a + b
